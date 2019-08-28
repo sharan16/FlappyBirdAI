d5d0aa8e6ca91bc170d189ea96423a1fb56f01c0
Sharan Somaskanthan <kingsharan02161999@gmail.com>
2019-08-27 21:09:11 -0400

    performance improvement for animations

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import time
 import neat
 import os
 import random
+import math
 
 WIN_W = 600
 WIN_H = 800
@@ -16,7 +17,6 @@ class Bird:
     IMGS = IMGS_BIRD
     MAX_ROT = 25
     VEL_ROT = 20
-    ANIMATION_TIME = 5
     
     def __init__(self,x,y):
         self.x = x
@@ -30,7 +30,7 @@ class Bird:
     
     def update(self):
         self.tick_count += 1
-        d = self.vel*self.tick_count + 1.5*(self.tick_count**2)
+        d = self.vel*self.tick_count + 2.5*(self.tick_count**2)
         if d > 16:
             d = 16
         elif d<0:
@@ -42,24 +42,17 @@ class Bird:
         self.tick_count = 0 
         self.height = self.y
     
-    def draw(self,win):
+    def draw(self,win): # Drawing the bird
         self.img_count += 1
-        if self.img_count < self.ANIMATION_TIME:
-            self.img = self.IMGS[0]
-        elif self.img_count < self.ANIMATION_TIME*2:
-            self.img = self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME*3:
-            self.img = self.IMGS[2]
-        elif self.img_count < self.ANIMATION_TIME*4:
-            self.img = self.IMGS[1]
-        elif self.img_count == self.ANIMATION_TIME*4 + 1:
-            self.img = self.IMGS[0]
-            self.img_count = 0
-        
+        self.img = self.IMGS[math.floor(self.img_count/5)]
+
         if self.tilt < -80:
             self.img = self.IMGS[1]
-            self.img_count = self.ANIMATION_TIME*2
+            self.img_count = 0
         
+        if self.img_count > 13:
+            self.img_count = 0
+
         rotated_img = pygame.transform.rotate(self.img,self.tilt)
         new_rect = rotated_img.get_rect(center = self.img.get_rect(topleft = (self.x,self.y)).center)
         win.blit(rotated_img,new_rect.topleft)
@@ -69,6 +62,7 @@ class Bird:
 
 def draw_window(win,bird):
     win.blit(IMG_BG,(0,0))
+    bird.update()
     bird.draw(win)
     pygame.display.update()
 
@@ -78,9 +72,9 @@ def main():
     run = True
     while run:
         pygame.event.get()
-        # for event in pygame.event.get():
-            # if event.type == pygame.QUIT:
-            #     run = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
         draw_window(win, bird)
     pygame.quit()
     quit()
